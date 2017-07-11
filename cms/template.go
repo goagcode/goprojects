078f968ffa8594d0dca3d38b5d9b8611665e0af8
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-07-10 20:25:09 -0500

    templates created

--- cms/template.go
+++ cms/template.go
@@ -1,11 +1,26 @@
 package cms
 
-import "html/template"
+import (
+	"html/template"
+	"time"
+)
 
 // Tmpl is an exported variable
 var Tmpl = template.Must(template.ParseGlob("../templates/*"))
 
 type Page struct {
 	Title   string
+	Content string
+	Posts   []*Post
+}
+
+type Post struct {
+	Title         string
+	Content       string
+	DatePublished time.Time
+	Comments      []*Comment
+}
+
+type Comment struct {
 	Content string
 }
