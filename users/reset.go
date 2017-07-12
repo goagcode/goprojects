506f52e7a5d4d0dab78976fe82a98a572ac8501a
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-07-11 19:11:52 -0500

    passwordForm hardcode created

--- users/reset.go
+++ users/reset.go
@@ -3,7 +3,35 @@ package users
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"net/smtp"
+	"os"
 )
+
+// validTokens is just an array of currently valid tokens.
+var validTokens []string
+
+const passwordForm = `
+<h1>Enter you new password</h1>
+<form action="/reset" method="POST">
+	<input type="hidden" name="email" value="{{ . }}" required/>
+	<label for="password">Password</label>
+	<input type="password" name="password" required/>
+	<input type="submit" value="Submit" />
+</form>
+`
+
+// SendPasswordResetEmail sends a password reset email to the given user.
+func SendPasswordResetEmail(email string) error {
+	token := string(genRandBytes())
+	validTokens = append(validTokens, token)
+	resetLink := "http://localhost:3000/reset?user=" + email + "&token=" + token
+
+	username := os.Getenv("GMAIL_USERNAME")
+	password := os.Getenv("GMAIL_PASSWORD")
+	auth := smtp.PlainAuth("smpt.gmail.com:587", auth, username, "smpt.gmail.com")
+
+	return smtp.SendMail("smpt.gmail.com:587", auth, username, []string{email}, []byte(resetLink))
+}
 
 // genRandBytes generates a 32 bytes long string of random bytes.
 func genRandBytes() []byte {
