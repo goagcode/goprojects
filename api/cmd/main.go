84450e50729b44c966f5f83f64a9b3aafe9c61e7
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-07-12 15:51:01 -0500

    endpoints added into the server http

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gorilla/mux"
+	"github.com/miguellgt/goprojects/api"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/pages", api.CreatePage).Methods("POST")
+	router.HandleFunc("/pages", api.GetPages).Methods("GET")
+	router.HandleFunc("/pages/{pageId}", api.GetPage).Methods("GET")
+
+	http.ListenAndServe(":3000", router)
 }
