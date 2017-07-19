2ef1de52d8c35ea36752cf6ab2ce9d83556fbd30
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-07-19 17:47:15 -0500

    decode fuction is created

--- tdd/jsondecoding/main.go
+++ tdd/jsondecoding/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type Post struct {
 	Id       int       `json:"id"`
 	Content  string    `json:"content"`
@@ -16,4 +22,27 @@ type Comment struct {
 	Id      int    `json:"id"`
 	Content string `json:"content"`
 	Author  string `json:"author"`
+}
+
+func decode(filename string) (post Post, err error) {
+	jsonFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error opeing JSON file:", err)
+		return
+	}
+	defer jsonFile.Close()
+	decoder := json.NewDecoder(jsonFile)
+	err = decoder.Decode(&post)
+	if err != nil {
+		fmt.Println("Erro decoding JSON: ", err)
+		return
+	}
+	return
+}
+
+func main() {
+	_, err := decode("post.json")
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 }
