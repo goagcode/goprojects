03a386f9151458b7b8f35d058960b268d15413d6
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-07-19 17:41:37 -0500

    models were creatd

---
+++ tdd/jsondecoding/main.go
@@ -0,0 +1,19 @@
+package main
+
+type Post struct {
+	Id       int       `json:"id"`
+	Content  string    `json:"content"`
+	Author   string    `json:"author"`
+	Comments []Comment `json:"comments"`
+}
+
+type Author struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Comment struct {
+	Id      int    `json:"id"`
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
