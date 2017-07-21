8a0fc1d47bf81761beaa5ce4ece2af5aa29d464e
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-07-21 16:46:10 -0500

    first scenario added to check just status code

--- tdd/http/main_test.go
+++ tdd/http/main_test.go
@@ -3,13 +3,12 @@ package main_test
 import (
 	"net/http"
 	"net/http/httptest"
-	"strings"
 
 	"github.com/gorilla/mux"
 	. "github.com/miguellgt/goprojects/tdd/http"
 
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 )
 
 type FakeOrder struct {
@@ -20,38 +19,27 @@ func (fo FakeOrder) Get() ([]Order, error) {
 	return fo.Orders, nil
 }
 
-var _ = Describe("Testing Order Resource", func() {
-	It("Get orders", func() {
+var _ = Describe("Get a post", func() {
+	var router *mux.Router
+	var w *httptest.ResponseRecorder
+	var order *FakeOrder
+
+	BeforeEach(func() {
+		order = &FakeOrder{}
 		// Create a multiplexer to run test on
-		router := mux.NewRouter()
-		// Captures return HTTP response
-		w := httptest.NewRecorder()
+		router = mux.NewRouter()
 		// Attaches handler you want to test
 		router.HandleFunc("/orders", HandlerOrders(&FakeOrder{})).Methods("GET")
+		// Captures return HTTP response
+		w = httptest.NewRecorder()
+	})
+
+	It("Get all orders", func() {
 		// Creates request to handler you want to test
 		r, _ := http.NewRequest("GET", "/orders", nil)
 		// Send request to tested handler
 		router.ServeHTTP(w, r)
-
 		// Check ResponseWriter for results
-		if w.Code != 200 {
-			GinkgoT().Errorf("Response code is %v", w.Code)
-		}
-	})
-
-	It("Put Order", func() {
-		router := mux.NewRouter()
-		router.HandleFunc("/orders", HandlerUpdateOrder).Methods("PUT")
-		w := httptest.NewRecorder()
-		order := strings.NewReader(`{
-			"client": "Luis Angel"
-		}`)
-		r, _ := http.NewRequest("PUT", "/orders", order)
-
-		router.ServeHTTP(w, r)
-
-		if w.Code != 204 {
-			GinkgoT().Errorf("Status code expected 204 but got %v", w.Code)
-		}
+		Expect(w.Code).To(Equal(200))
 	})
 })
