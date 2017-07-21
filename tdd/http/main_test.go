31704836b90d155062b2a76ee235d9242d77cb2e
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-07-21 16:52:36 -0500

    new scenario added to split code

--- tdd/http/main_test.go
+++ tdd/http/main_test.go
@@ -34,12 +34,18 @@ var _ = Describe("Get a post", func() {
 		w = httptest.NewRecorder()
 	})
 
-	It("Get all orders", func() {
-		// Creates request to handler you want to test
-		r, _ := http.NewRequest("GET", "/orders", nil)
-		// Send request to tested handler
-		router.ServeHTTP(w, r)
-		// Check ResponseWriter for results
-		Expect(w.Code).To(Equal(200))
+	Context("Get all orders", func() {
+		Context("When client send request GET /orders", func() {
+			BeforeEach(func() {
+				// Creates request to handler you want to test
+				r, _ := http.NewRequest("GET", "/orders", nil)
+				// Send request to tested handler
+				router.ServeHTTP(w, r)
+			})
+			It("then should get 200 status code", func() {
+				// Check ResponseWriter for results
+				Expect(w.Code).To(Equal(200))
+			})
+		})
 	})
 })
