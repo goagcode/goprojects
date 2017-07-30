4ad0f14ce38bd5e45ae59e242b14ca1f1d86655b
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-07-30 17:25:53 -0500

    unit test to root handler were refactored

--- dino/handlers/weportal_test.go
+++ dino/handlers/weportal_test.go
@@ -12,17 +12,31 @@ import (
 )
 
 var _ = Describe("Weportal", func() {
-	Describe("When client send request to GET / endpoint", func() {
-		It("should return a message 'Welcome to Dino Web Portal'", func() {
-			mux := mux.NewRouter()
-			mux.HandleFunc("/", handlers.RootHandler).Methods("GET")
 
-			w := httptest.NewRecorder()
-			r, _ := http.NewRequest("GET", "/", nil)
-			mux.ServeHTTP(w, r)
+	Describe("RootHandler", func() {
+		var router *mux.Router
+		var w *httptest.ResponseRecorder
 
-			Expect(w.Code).To(Equal(200))
-			Expect(string(w.Body.Bytes())).To(Equal("Welcome to Dino Web Portal"))
+		BeforeEach(func() {
+			router = mux.NewRouter()
+			router.HandleFunc("/", handlers.RootHandler).Methods("GET")
+			w = httptest.NewRecorder()
+		})
+
+		Context("When client send a request to GET / endpoint", func() {
+			BeforeEach(func() {
+				r, _ := http.NewRequest("GET", "/", nil)
+				router.ServeHTTP(w, r)
+			})
+
+			It("should return a message 'Welcome to Dino Web Portal'", func() {
+				Expect(w.Code).To(Equal(200))
+				Expect(string(w.Body.Bytes())).To(Equal("Welcome to Dino Web Portal"))
+			})
+
+			It("should return 200 statuc code", func() {
+				Expect(w.Code).To(Equal(200))
+			})
 		})
 	})
 })
