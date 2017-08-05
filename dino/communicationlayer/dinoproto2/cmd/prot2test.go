df90164a875a7f3e3af4a760e10e6af07cabf0fd
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-08-04 20:04:11 -0500

    tcp server create fo proto2

---
+++ dino/communicationlayer/dinoproto2/cmd/prot2test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+)
+
+/*
+	1.- We will serialize some data via proto2
+	2.- We will send data via TCP to a different service
+	3.- We will deserialize tha data via proto2, and print out the extracted values
+
+	A.- A TCP client needs to be written to send tha data
+	B.- A TCP server to receive the data
+*/
+
+func main() {
+	op := flag.String("op", "s", "s for server, and c for client")
+	flag.Parse()
+	switch strings.ToLower(*op) {
+	case "s":
+		RunProto2Server()
+	case "c":
+		fmt.Println("for client")
+	}
+}
+
+func RunProto2Server() {
+	l, err := net.Listen("tcp", ":8282")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer l.Close()
+		go func(conn net.Conn) {
+			defer conn.Close()
+			data, err := ioutil.ReadAll(conn)
+			if err != nil {
+				return
+			}
+			a := &dinoproto2.Animal{}
+			proto.Unmarshal(data, a)
+			fmt.Println(a)
+		}(c)
+	}
+}
