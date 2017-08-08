32ca1cfa123ca0dd8e3a3a7d6e5a47533be5ccd0
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-08-07 19:22:30 -0500

    function to create Animal type to grpc

--- dino/communicationlayer/dinogrpc/dinogrpc.go
+++ dino/communicationlayer/dinogrpc/dinogrpc.go
@@ -3,6 +3,8 @@ package dinogrpc
 import (
 	"fmt"
 
+	context "golang.org/x/net/context"
+
 	"github.com/miguellgt/goprojects/dino/databaselayer"
 )
 
@@ -17,5 +19,20 @@ func NewDinoGrpcServer(dbtype uint8, connstring string) (*DinoGrpcServer, error)
 	}
 	return &DinoGrpcServer{
 		dbHandler: handler,
+	}, nil
+}
+
+func (server *DinoGrpcServer) GetAnimal(ctx context.Context, r *Request) (*Animal, error) {
+	animal, err := server.dbHandler.GetDynoByNickname(r.GetNickname())
+	return convertToDinoGRPCAnimal(animal), err
+}
+
+func convertToDinoGRPCAnimal(animal databaselayer.Animal) *Animal {
+	return &Animal{
+		Id:         int32(animal.ID),
+		AnimalType: animal.AnimalType,
+		Nickname:   animal.Nickname,
+		Zone:       int32(animal.Zone),
+		Age:        int32(animal.Age),
 	}
 }
