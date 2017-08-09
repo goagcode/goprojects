ec32177d796b7b87cbbc5f4c4ce4da97845b3d27
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-08-09 18:38:40 -0500

    GetCustomer method is implemented to RPC server

--- grpc/server/main.go
+++ grpc/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	pb "github.com/miguellgt/goprojects/grpc/customer"
 	"golang.org/x/net/context"
@@ -19,6 +20,20 @@ type server struct {
 func (s *server) CreateCustomer(ctx context.Context, in *pb.CustomerRequest) (*pb.CustomerResponse, error) {
 	s.savedCustomers = append(s.savedCustomers, in)
 	return &pb.CustomerResponse{Id: in.Id, Success: true}, nil
+}
+
+func (s *server) GetCustomers(filter *pb.CustomerFilter, stream pb.Customer_GetCustomersServer) error {
+	for _, customer := range s.savedCustomers {
+		if filter.Keyword != "" {
+			if !strings.Contains(customer.Name, filter.Keyword) {
+				continue
+			}
+		}
+		if err := stream.Send(customer); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func main() {
