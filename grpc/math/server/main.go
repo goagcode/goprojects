f26ce8fa7f78c43562388adf937dbd2c6152286f
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-08-11 17:58:12 -0500

    Divide procedure implemented into the mathServer

--- grpc/math/server/main.go
+++ grpc/math/server/main.go
@@ -1,6 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+
+	ms "github.com/miguellgt/goprojects/grpc/math/math_service"
+
+	context "golang.org/x/net/context"
+)
+
+type mathServer struct{}
+
+func (m *mathServer) Divide(ctx context.Context, in *ms.Operands) (*ms.Result, error) {
+	// Handle divide by zero
+	if in.Dividend == 0 {
+		return nil, grpc.Errorf(codes.InvalidArgument, "Cannot divide by zero")
+	}
+	// cast, divide, return
+	return &ms.Result{Quotient: float32(in.Dividend) / float32(in.Divisor)}, nil
+}
 
 func main() {
 	fmt.Println("Running tcp server")
