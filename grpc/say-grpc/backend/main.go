48103c220a578cd0d13154d8fc88df9a1c0ae5c1
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-08-12 21:27:09 -0500

    gRPC server to TextToSpeech service created

--- grpc/say-grpc/backend/main.go
+++ grpc/say-grpc/backend/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-	"log"
-	"os"
-	"os/exec"
+	"flag"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"github.com/Sirupsen/logrus"
+	pb "github.com/miguellgt/goprojects/grpc/say-grpc/api"
 )
 
 func main() {
-	cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+	port := flag.Int("p", 8080, "port to listen to")
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		logrus.Fatal("could not listen to port %d: %v", *port, err)
 	}
+
+	s := grpc.NewServer()
+	pb.RegisterTextToSpeechServer(s, server{})
 }
+
+// cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
+// cmd.Stdout = os.Stdout
+// cmd.Stderr = os.Stderr
+// if err := cmd.Run(); err != nil {
+// 	log.Fatal(err)
+// }
