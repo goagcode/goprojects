321306eef57ab8742e43f1829661720f13d2e22e
Miguel Angel Galicia <miguel.galiciatorrez@gmail.com>
2017-08-12 21:33:41 -0500

    service is created but not implemented

--- grpc/say-grpc/backend/main.go
+++ grpc/say-grpc/backend/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	pb "github.com/miguellgt/goprojects/grpc/say-grpc/api"
+	"golang.org/x/net/context"
 )
 
 func main() {
@@ -17,11 +18,22 @@ func main() {
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
-		logrus.Fatal("could not listen to port %d: %v", *port, err)
+		logrus.Fatalf("could not listen to port %d: %v", *port, err)
 	}
 
 	s := grpc.NewServer()
 	pb.RegisterTextToSpeechServer(s, server{})
+	err = s.Serve(lis)
+	if err != nil {
+		logrus.Fatalf("could not serve: %v", err)
+	}
+}
+
+type server struct{}
+
+func (server) Say(ctx context.Context, in *pb.Text) (*pb.Speech, error) {
+	return nil, fmt.Errorf("not implemented")
+
 }
 
 // cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
